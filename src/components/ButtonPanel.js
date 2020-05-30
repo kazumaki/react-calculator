dbdc0485431ea8d57541079d17f65a4b5b4b8a90
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-05-30 14:35:41 -0300

    Fix linter errors

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -8,35 +8,35 @@ const rowStyle = {
 
 const symbols = ['AC', '+/-', '%', '÷', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
-const ButtonPanel = () => (
-  <div id="button-panel">
-    {getGroups()}
-  </div>
-);
-
 const getGroups = () => {
   let buttons = [];
-  let groups = [];
+  const groups = [];
   let currentGroup = 0;
-  for(let i = 0; i < symbols.length; i++) {
-    if((i + 1) % 4 === 0 || symbols[i] === '=') {
-      buttons.push(<Button name={symbols[i]} />)
-      groups.push(<div id={`group-${currentGroup}`} style={rowStyle}>{buttons}</div>)
+  for (let i = 0; i < symbols.length; i += 1) {
+    if ((i + 1) % 4 === 0 || symbols[i] === '=') {
+      buttons.push(<Button name={symbols[i]} />);
+      groups.push(<div id={`group-${currentGroup}`} style={rowStyle}>{buttons}</div>);
       currentGroup++;
       buttons = [];
     }
     else 
     {
       if (symbols[i] === '0') {
-        buttons.push(<Button name={symbols[i]} color="light-gray" wide/>)
+        buttons.push(<Button name={symbols[i]} color="light-gray" wide/>);
       }
       else
       {
-        buttons.push(<Button name={symbols[i]} color="light-gray" />)
+        buttons.push(<Button name={symbols[i]} color="light-gray" />);
       }
     }
   }
   return groups;
 }
+
+const ButtonPanel = () => (
+  <div id="button-panel">
+    {getGroups()}
+  </div>
+);
 
 export default ButtonPanel;
