c2ba7b3b7d5f8e23d9a32c1a14a7908c5e61477d
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-06-01 04:39:29 -0300

    Add check for division by 0; Add check for . button press and not allow more than one; Add totalOperate method to operate the total value

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,33 +1,60 @@
 import operate from './operate';
 
 const operators = ['+', '-', 'X', '÷', '%'];
+let resultState = 'total';
+
+const totalOperate = (data) => {
+  if (data.operation) {
+    if (data.operation === '÷') {
+      if (data.next == 0) {
+        return null;
+      } else if (data.next) {
+        return operate(data.total || 0, data.next, data.operation);
+      } else {
+        return null;
+      }
+    }
+    return operate(data.total || 0, data.next || 0, data.operation);
+  } else {
+    return data.next;
+  }
+}
 
 const calculate = (data, button) => {
-  const returnData = {
-    total: data.total,
-    next: data.next,
-    operation: data.operation,
-    display: data.display,
-  };
+  const returnData = Object.assign({}, data);
 
   if (operators.includes(button)) {
-    returnData.total = operate(data.total || 0, data.next || 0, button);
+    returnData.total = totalOperate(returnData);
     returnData.next = 0;
     returnData.operation = button;
-    returnData.display = returnData.total;
+    resultState = 'total';
   } else if (button === '=') {
-    returnData.total = operate(data.total || 0, data.next || 0, data.operation);
+    returnData.total = totalOperate(returnData);
     returnData.next = 0;
-    returnData.display = returnData.total;
+    returnData.result = returnData.total;
+    resultState = 'total';
   } else if (button === '+/-') {
     returnData.total *= -1;
     returnData.next *= -1;
-    returnData.display = returnData.total;
+  } else if (button === 'AC') {
+    returnData.total = 0;
+    returnData.next = 0;
+    resultState = 'total';
+  } else if (button === '.') {
+    if (returnData.next) {
+      if (!returnData.next.includes('.')) {
+        returnData.next = returnData.next + button;
+      }
+    } else {
+      returnData.next = '0' + button;
+    }
+    resultState = 'next';
   } else {
     returnData.next = data.next ? data.next + button : button;
-    returnData.display = returnData.next;
+    returnData.result = returnData.next;
+    resultState = 'next';
   }
-
+  returnData.result = resultState === 'total' ? returnData.total : returnData.next;
   return returnData;
 };
 
